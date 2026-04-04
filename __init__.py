dd9b892efe7eb8ed8a4df2edca6825bc219b5ef6
vashtera <raul.aytbaev@gmail.com>
2026-04-04 14:39:11 +0500

    code is work

--- __init__.py
+++ __init__.py
@@ -2,25 +2,20 @@ from datetime import datetime
 from datetime import time
 class not_smoke:
     def __init__(self, start_date, price):
-        self.start_date = start_date
         start_date = datetime.strptime(start_date,"%d/%m/%y")
+        self.start_date = start_date
         self.price = price
 
-    def starting_date(self, result):
-        self.result = result
-        result = self.start_date - datetime.now()
-        return result
-    
-    def money(self, saved_money):
-        self.saved_money = saved_money
-        saved_money = self.price * self.result
-        return
+    def calculate_all(self):
+        self.result = datetime.now() - self.start_date 
+        self.saved_money = self.result.days * self.price 
     
     def info(self):
+        self.calculate_all()
         print(f"Бросил курить: {self.start_date}, сэкономил: {self.saved_money}, не куришь уже: {self.result}")
 
 erbol = not_smoke('3/4/26', 1250)
-erbol.info()
+print(erbol.info())
 
 
     
