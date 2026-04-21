6ad47f00e6bedf7636aa44d9cd86fd747fb193de
vashtera <raul.aytbaev@gmail.com>
2026-04-21 14:57:22 +0500

    Entered a file for save date

--- __init__.py
+++ __init__.py
@@ -5,6 +5,8 @@ class not_smoke:
         start_date = datetime.strptime(start_date,"%d/%m/%y")
         self.start_date = start_date
         self.price = price
+        with open("start_date.txt", 'w') as start:
+            start.write(start_date)
     #This func just defines start date
     def starting_date(self):
         result = datetime.now() - self.start_date
@@ -14,15 +16,18 @@ class not_smoke:
         self.saved_money = self.price * self.result.days
     #This func is restart date, and resets saved money
     def restart(self):
-        self.start_date = datetime.now()
+        with open("start_date.txt", 'w') as start:
+            start.write(datetime.now)
     #Info method
     def info(self):
         self.starting_date()
         self.money()
         print(f"Бросил курить: {self.start_date}, сэкономил: {self.saved_money}, не куришь уже: {self.result}")
 
-erbol = not_smoke('3/4/26', 1250)
-erbol.restart
+start_date = input("Please enter your starting date(Day/Month/Year): ")
+price_of_cigarettes = int(input("Please enter the price of cigarettes: "))
+erbol = not_smoke(start_date, price_of_cigarettes)
+#erbol.restart
 erbol.info()
 
 
