ecbeed10e458222a79796c61f6652918e4ba489e
vashtera <raul.aytbaev@gmail.com>
2026-04-21 16:33:25 +0500

    Fixed the code

--- __init__.py
+++ __init__.py
@@ -2,29 +2,30 @@ from datetime import datetime
 from datetime import time
 class not_smoke:
     def __init__(self, start_date, price):
-        start_date = datetime.strptime(start_date,"%d/%m/%y")
-        self.start_date = start_date
         self.price = price
-        with open("start_date.txt", 'w') as start:
-            start.write(str(start_date))
+        start_date = datetime.strptime(start_date,"%d/%m/%y")
+        with open("Not-smoke/start_date.txt", 'w') as start:
+            start.write(start_date.strftime("%d/%m/%y"))
+        self.start_date = start_date
     #This func just defines start date
     def starting_date(self):
-        with open("start_date.txt", 'r') as start:
+        with open("Not-smoke/start_date.txt", 'r') as start:
             new_date = start.read()
-        result = datetime.strptime(new_date) - self.start_date
+        result = datetime.now() - datetime.strptime(new_date, "%d/%m/%y")
         self.result = result
     #This func just calc how much money I saved
     def money(self):
         self.saved_money = self.price * self.result.days
     #This func is restart date, and resets saved money
     def restart(self):
-        with open("start_date.txt", 'w') as start:
-            start.write(str(datetime.now()))
+        self.start_date = datetime.now()
+        with open("Not-smoke/start_date.txt", 'w') as start:
+            start.write(datetime.now().strftime("%d/%m/%y"))
     #Info method
     def info(self):
         self.starting_date()
         self.money()
-        print(f"Бросил курить: {self.start_date}, сэкономил: {self.saved_money}, не куришь уже: {self.result}")
+        print(f"Бросил курить: {self.start_date.strftime("%d/%m/%y")}, сэкономил: {self.saved_money}, не куришь уже: {self.result}")
 
 start_date = input("Please enter your starting date(Day/Month/Year): ")
 price_of_cigarettes = int(input("Please enter the price of cigarettes: "))
